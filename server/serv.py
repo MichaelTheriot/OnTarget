6be5e749a7a84cf027da1460d720e14e467ff0d4
Austin Stanley <r.austin.stanley@gmail.com>
2016-05-02 01:12:28 -0500

    implement async

--- server/serv.py
+++ server/serv.py
@@ -8,23 +8,17 @@ import platform
 import websockets
 import asyncio
 import glob
+import signal
+import functools
 from tools import *
 from serial.serialutil import SerialException
 from serial.tools.list_ports import comports
-from queue import Queue
 
-async def transmit(websocket, path):
-    while True:
-        try:
-            coords = coord_q.get(False)
-        except queue.Empty:
-            continue
-        await websocket.send('{:.2f},{:.2f},{}\n'.format(coords.x, coords.y, time.time() * 1000))
-        coord_q.task_done()
-
+def terminate():
+    sys.exit('\nexiting...')
 
 def get_pcc(times):
-    '''Calculate a point and 2 circles for Apollonius algorithm'''
+    """Calculate a point and 2 circles for Apollonius algorithm"""
     speed_of_sound = 0.00112533 # feet/microsecond
     mic_coords = (Point(0, 4), Point(2, 1), Point(-3, 2))
     first_mic = times.index(min(times))
@@ -39,7 +33,49 @@ def get_pcc(times):
 
     return p, circles[0], circles[1]
 
-def main(argv):
+
+async def consume(websocket, path):
+    if len(sys.argv) > 1 and sys.argv[1] == '-g':
+        gui = True
+    else:
+        gui = False
+
+    while True:
+        try:
+            await coords = coord_q.get()
+        except asyncio.QueueEmpty:
+            continue
+        if gui:
+            await websocket.send('{:.2f},{:.2f},{}\n'.format(coords.x, coords.y, time.time() * 1000))
+        print('({:.2f}, {:.2f})'.format(coords.x, coords.y))
+
+        if os.path.ismount('/mnt/usb'):
+            with open('/mnt/usb/data.csv', 'a') as f:
+                f.write(str(coords.x) + ',' + str(coords.y) + ',' + 
+                        str(time.time() * 1000) + '\n')
+
+
+async def produce():
+    try:
+        msg = ser.readline().decode()
+    except SerialException:
+        print('Device disconnected. Exiting...')
+        sys.exit()
+
+    if msg:
+        times = [int(time) for time in msg.split()]
+
+        p, c1, c2 = get_pcc(times)
+        coords = find_target(p, c1, c2)
+
+        if coords:
+            coord_q.put_nowait(coords)
+
+        else:
+            print('Calculation aborted')
+
+if __name__ == '__main__':
+    # establish serial connection
     if platform.system() == 'Linux':
         ports = glob.glob('/dev/tty[A-Za-z]*')
     else if platform.system() == 'Windows': 
@@ -58,48 +94,20 @@ def main(argv):
     if not ser:
         sys.exit('Serial connection failed.')
 
-    if len(argv) > 1 and argv[1] == '-g':
-        gui = True
-    else:
-        gui = False
+    loop = asyncio.get_event_loop()
 
-    if gui:
-        coord_q = Queue()
-        start_server = websockets.serve(transmit, '', 5001)
-        asyncio.get_event_loop().run_until_complete(start_server)
-        asyncio.get_event_loop().run_forever()
+    coord_q = asyncio.Queue()
+    start_server = websockets.serve(consume, '', 5001)
+    asyncio.ensure_future(start_server)
+    asyncio.ensure_future(produce())
+
+    print('Listening on serial port. Ctrl-c to quit.')
 
     try:
-        print('Listening on serial port. Ctrl-c to quit.')
-        while True:
-            try:
-                msg = ser.readline().decode()
-            except SerialException:
-                print('Device disconnected. Exiting...')
-                sys.exit()
-
-            if msg:
-                times = [int(time) for time in msg.split()]
-
-                p, c1, c2 = get_pcc(times)
-                coords = find_target(p, c1, c2)
-
-                if coords:
-                    print('({:.2f}, {:.2f})'.format(coords.x, coords.y))
-
-                    if gui:
-                        coord_q.put(coords, False)
-
-                    if os.path.ismount('/mnt/usb'):
-                        with open('/mnt/usb/data.csv', 'a') as f:
-                            f.write(str(coords.x) + ',' + str(coords.y) + ',' + 
-                                    str(time.time() * 1000) + '\n')
-                else:
-                    print('Calculation aborted')
-                    
+        loop.run_forever()
     except KeyboardInterrupt:
-        print('\nexiting...')
+        loop.close()
+        sys.exit('\nExiting...')
+    finally:
+        loop.close()
 
-
-if __name__ == '__main__':
-    main(sys.argv)
