3efdb8b2227e19fafc0b91cb0604cf85f3a423b5
Michael Theriot <michael.lee.theriot@gmail.com>
2016-05-06 00:21:15 -0500

    merge

--- server/serv.py
+++ server/serv.py
@@ -8,29 +8,24 @@ import platform
 import websockets
 import asyncio
 import glob
-<<<<<<< HEAD
-=======
 import signal
 import functools
 import concurrent
->>>>>>> 4ec7c2576f8edeb933eebd1bb28cc9ed119f3b8f
 from tools import *
 from serial.serialutil import SerialException
 from serial.tools.list_ports import comports
-from queue import Queue
 
-async def transmit(websocket, path):
-    while True:
-        try:
-            coords = coord_q.get(False)
-        except queue.Empty:
-            continue
-        await websocket.send('{:.2f},{:.2f},{}\n'.format(coords.x, coords.y, time.time() * 1000))
-        coord_q.task_done()
+class Impact(object):
+    def __init__(self, coords, time):
+        self.coords = coords
+        self.time = time
 
+def terminate():
+    loop.stop()
+    sys.exit('\nexiting...')
 
 def get_pcc(times):
-    '''Calculate a point and 2 circles for Apollonius algorithm'''
+    """Calculate a point and 2 circles for Apollonius algorithm"""
     speed_of_sound = 0.00112533 # feet/microsecond
     mic_coords = (Point(0, 4), Point(2, 1), Point(-3, 2))
     first_mic = times.index(min(times))
@@ -45,9 +40,6 @@ def get_pcc(times):
 
     return p, circles[0], circles[1]
 
-<<<<<<< HEAD
-def main(argv):
-=======
 def get_msg():
     try:
         msg = ser.readline()
@@ -107,10 +99,9 @@ async def produce():
 
 if __name__ == '__main__':
     # establish serial connection
->>>>>>> 4ec7c2576f8edeb933eebd1bb28cc9ed119f3b8f
     if platform.system() == 'Linux':
         ports = glob.glob('/dev/tty[A-Za-z]*')
-    else if platform.system() == 'Windows': 
+    elif platform.system() == 'Windows': 
         ports = ['COM{}'.format(i + 1) for i in range(256)]
     else:
         sys.exit('Unsupported platform')
@@ -126,12 +117,6 @@ if __name__ == '__main__':
     if not ser:
         sys.exit('Serial connection failed.')
 
-<<<<<<< HEAD
-    if len(argv) > 1 and argv[1] == '-g':
-        gui = True
-    else:
-        gui = False
-=======
     HOST = '127.0.0.1'
     PORT = 5001
 
@@ -140,45 +125,14 @@ if __name__ == '__main__':
     start_server = websockets.serve(transmit, HOST, PORT)
     asyncio.ensure_future(produce())
     asyncio.ensure_future(read_serial_async())
->>>>>>> 4ec7c2576f8edeb933eebd1bb28cc9ed119f3b8f
 
-    if gui:
-        coord_q = Queue()
-        start_server = websockets.serve(transmit, '', 5001)
-        asyncio.get_event_loop().run_until_complete(start_server)
-        asyncio.get_event_loop().run_forever()
+    print('Listening on serial port. Ctrl-c to quit.')
 
     try:
-        print('Listening on serial port. Ctrl-c to quit.')
-        while True:
-            try:
-                msg = ser.readline().decode()
-            except SerialException:
-                print('Device disconnected. Exiting...')
-                sys.exit()
-
-            if msg:
-                times = [int(time) for time in msg.split()]
-
-                p, c1, c2 = get_pcc(times)
-                coords = find_target(p, c1, c2)
-
-                if coords:
-                    print('({:.2f}, {:.2f})'.format(coords.x, coords.y))
-
-                    if gui:
-                        coord_q.put(coords, False)
-
-                    if os.path.ismount('/mnt/usb'):
-                        with open('/mnt/usb/data.csv', 'a') as f:
-                            f.write(str(coords.x) + ',' + str(coords.y) + ',' + 
-                                    str(time.time() * 1000) + '\n')
-                else:
-                    print('Calculation aborted')
-                    
+        loop.run_until_complete(start_server)
+        loop.run_forever()
     except KeyboardInterrupt:
-        print('\nexiting...')
-
-
-if __name__ == '__main__':
-    main(sys.argv)
+        loop.close()
+        sys.exit('\nExiting...')
+    finally:
+        loop.close()
